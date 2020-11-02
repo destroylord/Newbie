85ce08d317a417eaebfb1e9bae2f39c4bbe7dba3
destroylord <masapin68@gmail.com>
2020-11-02 21:40:21 +0700

    System input output menggunakan BufferedReader

--- Newbie.java
+++ Newbie.java
@@ -1,8 +1,31 @@
+import java.io.BUfferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class Newbie {
 
     public static void main(String[] args) {
-        
-        System.out.print("Hello World");
 
+        InputStreamReader sR = new InputStreamReader(System.in);
+        BUfferedReader bR = new BUfferedReader(sR);
+
+
+        System.out.println("Program penjumlahan sangat sederhana");
+
+        int value = 0;
+        int anotherValue = 0;
+        
+        try{
+            System.out.print("Masukkan angka pertama : ");
+            value = Integer.parseInt(bR.readLine());
+            System.out.print("Masukkan angka kedua : ");
+            anotherValue = Integer.parseInt(bR.readLine());
+        }catch(IOException e){
+            e.printstackTrace();
+        }
+
+        int result = value + anotherValue;
+        System.out.print("Hasilnya adalah "+ result);
     }
 }
